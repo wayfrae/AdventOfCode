f056506d29a036a32173f7370dc4f8f03f8ed49b
wayfrae <frazier.zach@gmail.com>
2018-12-04 23:16:21 -0700

    in progress of part 2

--- AdventOfCode_Day-1/Program.cs
+++ AdventOfCode_Day-1/Program.cs
@@ -11,15 +11,43 @@ namespace AdventOfCode_Day_1
         static void Main(string[] args)
         {
             List<int> input = new List<int>();
+            int frequency = 0;
+            int firstRepeat = 1234567;
+            bool keepLooping = true;
+            Dictionary<int, int> frequencyHistory = new Dictionary<int,int>();
 
-            // first read input till there are nonempty items, means they are not null and not ""
-            // also add read item to list do not need to read it again    
+            Console.WriteLine("Paste your list of frequencies: ");
+
+
             string line;
             while ((line = Console.ReadLine()) != null && line != "")
             {
-                input.Add(int.Parse(line));
+                input.Add(int.Parse(line));     
+                
             }
+            while (keepLooping)
+            {
+                for (int i = 0; i < input.Count; i++)
+                {
+                    if (frequencyHistory.ContainsKey(frequency))
+                    {
+                        frequencyHistory[frequency]++;
+                        keepLooping = false;
+                        break;
+                    }
+                    else
+                    {
+                        frequencyHistory.Add(frequency, 1);
+                    }
+                    frequency += int.Parse(line);
+                }
+            }
+            
 
+            Console.WriteLine("Frequency is " + frequency);
+            Console.WriteLine("Repeat Frequency is " + firstRepeat);
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
 
         }
     }
