d8e6088cdad417a3b790f2ba0847877f7745b6bd
wayfrae <frazier.zach@gmail.com>
2018-12-04 22:40:53 -0700

    Add project files.

---
+++ AdventOfCode_Day-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int>();
+
+            // first read input till there are nonempty items, means they are not null and not ""
+            // also add read item to list do not need to read it again    
+            string line;
+            while ((line = Console.ReadLine()) != null && line != "")
+            {
+                input.Add(int.Parse(line));
+            }
+
+
+        }
+    }
+}
